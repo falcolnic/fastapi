fd09774b0df2107a733e051a1154eb5849ed9093
falcolnic <mykh.yano@gmail.com>
2024-06-14 16:56:59 +0300

    Auth

---
+++ auth.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from pydantic import BaseModel
+from fastapi import FastAPI, APIRouter
+
+ 
+app = FastAPI()
+router = APIRouter()
+
+
+class User(BaseModel):
+    username: str
+    mail: str
+    phone: str
+    password: str
+
+@router.post("/register", tags=["auth"], status_code=200)
+async def register(user: User):
+    return "User, " + user.username + " with " + user.mail + " registered"
+    
+
+
+@router.post("/login", tags=["auth"], status_code=200)
+async def login(user: User):
+    return "User, " + user.username + " loggwed in"
+
+
+
+app.include_router(router)
---
+++ main.py
@@ -0,0 +1,12 @@
+from contextlib import asynccontextmanager
+from auth import *
+from fastapi import FastAPI, APIRouter
+
+
+@asynccontextmanager
+async def get_app():
+    app = FastAPI()
+    router = APIRouter()
+    app.include_router(router)
+    yield app
+    
